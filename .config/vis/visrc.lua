9662cde52a09f7d9283a634f25e7a547cd95efd1
zacc <zacc23@airmail.cc>
2020-09-07 12:04:22 -0400

    Add config header files

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,6 +1,5 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
-require('vis-title')
 
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command('set theme solarized')
